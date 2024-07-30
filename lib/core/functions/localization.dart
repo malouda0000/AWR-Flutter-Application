24fcd39d910f86c75976a11898a12fd50cc487b4
malouda0000 <hamam.hamza85@gmail.com>
2024-07-30 15:50:05 +0400

    adding the localization functions

---
+++ lib/core/functions/localization.dart
@@ -0,0 +1,39 @@
+import 'package:awr_flutter_application/core/constants/app_constants.dart';
+import 'package:awr_flutter_application/core/helpers/cash_helpers.dart';
+import 'package:awr_flutter_application/main.dart';
+
+// changeLocal()async {
+// Locale appLocal = await CacheHelper.getData(key: 'local');
+// // Locale appLocal = Locale('en');
+// print(appLocal);
+// }
+// Future<Locale>
+// getLocal() async {
+// // Locale appLocal =
+// //  if (await CacheHelper.getData(key: 'local') == null || await CacheHelper.getData(key: 'local') == "" ) {
+
+// //    Locale('en');
+
+// // }else {
+// //     Locale(await CacheHelper.getData(key: 'local') );
+// // };
+// // // Locale appLocal = Locale('en');
+
+// // print(appLocal);
+// // return appLocal;
+
+//   Locale initalLang =
+//       CacheHelper.getData(key: 'lang') == 'en' ? Locale('en') : Locale('ar');
+
+//   return initalLang;
+//   // String emailVerLang() {
+//   //   return CacheHelper.getData(key:'lang') == 'en' ? 'en' : 'ar';
+//   // }
+// }
+
+cachLocal({required String langCode}) async {
+  // Locale myLocale = Locale(langCode);
+  // Get.updateLocale(myLocale);
+  await CacheHelper.setData(key: AppConstants.cachedUserLang, value: langCode);
+  globalCachedUserLang = langCode; // RestartWidget.restartApp(context);
+}
