2a57df4773cfd39da9ec7449769a4263e7c64dff
malouda0000 <hamam.hamza85@gmail.com>
2024-07-31 20:20:55 +0400

    adding new images

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -7,6 +7,8 @@
 class AppImages {
 
   static const selShoHoriTextPng = "assets/images/selShoHoriText.png";
-
+  static const logoPng = "assets/images/awr_logo.png";
+  static const facebookPng = "assets/images/facebook.png";
+  static const googlePng = "assets/images/google.png";
 }
 
