e90899cfb18ec05d42a3bb871927b73072cf8783
malouda0000 <hamam.hamza85@gmail.com>
2024-08-01 04:29:21 +0400

    fixing app assets

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,14 +1,9 @@
-
-
-
-  //  ####            //
-  //  Gen App Images  //
+//  ####            //
+//  Gen App Images  //
 
 class AppImages {
-
-  static const selShoHoriTextPng = "assets/images/selShoHoriText.png";
+  // static const selShoHoriTextPng = "assets/images/selShoHoriText.png";
   static const logoPng = "assets/images/awr_logo.png";
   static const facebookPng = "assets/images/facebook.png";
   static const googlePng = "assets/images/google.png";
 }
-
