5343b574e9cec62014d1ffdec8f7bbbb9557c7d0
malouda0000 <hamam.hamza85@gmail.com>
2024-08-01 04:29:08 +0400

    configeuring the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:awr_flutter_application/generated/I10n.dart';
+import 'package:awr_flutter_application/view/auth/log_in_screen.dart';
+import 'package:awr_flutter_application/view/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -44,111 +46,78 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
-       supportedLocales: const [Locale('en'), Locale('ar')],
-  localizationsDelegates: const [
-    S.delegate,
-    GlobalMaterialLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-    GlobalCupertinoLocalizations.delegate,
-  ],
-  localeResolutionCallback: (deviceLocale, supportedLocales) {
-    for (var locale in supportedLocales) {
-      if (deviceLocale != null && deviceLocale.languageCode == locale.languageCode) {
-        return deviceLocale;
-      }
-    }
-    return supportedLocales.first;
-  },
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      localeResolutionCallback: (deviceLocale, supportedLocales) {
+        for (var locale in supportedLocales) {
+          if (deviceLocale != null &&
+              deviceLocale.languageCode == locale.languageCode) {
+            return deviceLocale;
+          }
+        }
+        return supportedLocales.first;
+      },
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({
+    super.key,
+    //  required this.title
+  });
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  // final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+// AppLocalizations appLocalizationsOfContext =  AppLocalizations.of(context)!;
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: SafeArea(
+            child: globalCachedUserToken == null || globalCachedUserToken == ""
+                ? const LogInScreen()
+                : const HomeScreen()));
+    // return const Scaffold(body: SafeArea(child: SignupScreen()));
+    // return const Scaffold(body: SafeArea(child: LogInScreen()));
+    // return const Scaffold(
+    // body: SafeArea(child: ChoseLanguageScreen(commingFromSettings: false)));
+    // return Scaffold(body: SafeArea(child: ErrorScreen(errorMessage: 'test')));
+    // return const Scaffold(body: SafeArea(child: HomeScreen()));
+    // return const Scaffold(body: SafeArea(child: ProductDetailsScreen()));
+    // return const Scaffold(body: SafeArea(child: ProductsScreen()));
+    // return const Scaffold(body: SafeArea(child: CheckOutScreen()));
+    // return const Scaffold(body: SafeArea(child: ChoosePaymentMethodScreen()));
+    // return const Scaffold(body: SafeArea(child: TrackOrderScreen()));
+    // return const Scaffold(body: SafeArea(child: EditUserProfileScreen()));
+    // return const Scaffold(body: SafeArea(child: UserLocationsScreen()));
+    // return const Scaffold(body: SafeArea(child: DiscountsScreen()));
+    // return Scaffold(
+    //     body: SafeArea(
+    //         child: PaymentScreen(
+    //   amount: 350
+    // )));
+
+    // return const Scaffold(
+    //     body: SafeArea(
+    //         child: ProductsOfCategoryScreen(
+    //   mainCategoryId: 1,
+    // )));
+
+    // return const Scaffold(body: SafeArea(child: SearchScreen()));
+    // return const Scaffold(body: SafeArea(child: UserLocationsScreen()));
+    // return const Scaffold(body: SafeArea(child: CreateNewLocationScreen()));
+    // return const Scaffold(body: SafeArea(child: CategoriesScreen()));
   }
 }
